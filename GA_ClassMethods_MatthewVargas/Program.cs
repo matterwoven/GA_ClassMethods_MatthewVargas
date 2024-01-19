36921910dbbacef63cb4ec6f6e39442fb1f09a8b
matterwoven <145516753+matterwoven@users.noreply.github.com>
2024-01-19 15:34:04 -0800

    Add project files.

---
+++ GA_ClassMethods_MatthewVargas/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace GA_ClassMethods_MatthewVargas
+{
+    internal class Program
+    {
+        //Matthew Vargas
+        //1/19/2024
+
+        static void Main(string[] args)
+        {
+            List<int> stuGrades = new List<int> {  40, 43, 89, 81, 95  };
+
+            Student schStudent = new Student("Will", stuGrades);
+
+            schStudent.AddGrade(78);
+            schStudent.AddGrade(200); //like the premade code, to test for abnormalities
+            schStudent.AddGrade(20);
+
+            schStudent.DisplayAllGrades();
+
+            schStudent.GetGrade();
+
+            schStudent.DisplayStudentInfo();
+
+            List<Student> randomStudents = GenerateRandomStudents();
+
+            randomStudents.Add(schStudent);
+
+            foreach (Student student in randomStudents)
+            {
+                student.DisplayStudentInfo();
+            }
+
+
+            Console.WriteLine("Hello, World!");
+        }
+
+        static List<Student> GenerateRandomStudents()
+        {
+
+            List<Student> students = new List<Student>();
+
+            Random random = new Random();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Student student = new Student ($"Student{i}");
+
+                for (int j = 0; j < 5; j++)
+                {
+                    student.ExamScores.Add(random.Next(0, 101));                
+                }
+
+                students.Add(student);
+            }
+
+            return students;
+        }
+
+
+    }
+}
---
+++ GA_ClassMethods_MatthewVargas/UnworkingMadeCode.cs
@@ -0,0 +1,122 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+     // Greened out for no conflicts, this was the code made before I pasted, as I was unable to see what was making the double show up in console, no need to debug, just wanted to show I did the work
+
+//namespace GA_ClassMethods_MatthewVargas
+//{
+//internal class Student
+//{
+//
+  //  public string Name { get; set; }
+  // public List<int> ExamScores { get; private set; }
+
+  //  public Student(string name)
+  //  {
+  //      Name = name;
+  //      ExamScores = new List<int>();
+  //  }
+
+  //  public Student(string name, List<int> examScores)
+  //  {
+  //      Name = name;
+  //      ExamScores = examScores;
+  //  }
+
+  //  public double CalculateAverageScore()       //
+  //  {
+  //      if (ExamScores.Count == 0)
+  //      {
+  //          return 0;
+  //      }
+  //      int totalScore = 0;
+  //      foreach (int score in ExamScores)
+  //      {
+  //          totalScore += score;
+  //      }
+
+  //     return (double)totalScore / ExamScores.Count;
+
+  //  }
+
+  //  public char GetGrade()
+  //  {
+  //      double averageScore = CalculateAverageScore();
+
+  //      if (averageScore >= 90)
+  //      {
+  //          return 'A';
+  //      }
+  //      else if (averageScore >= 80)
+  //      {
+  //          return 'B';
+  //      }
+  //      else if (averageScore >= 70)
+  //      {
+  //          return 'C';
+  //      }
+  //      else if (averageScore >= 60)
+  //      {
+  //          return 'D';
+  //      }
+  //      else
+  //      {
+  //          return 'F';
+  //      }
+  //  }
+
+  //   public void DisplayStudentInfo()
+  //  {
+  //      Console.WriteLine($"Student Name: {Name}");
+  //      Console.WriteLine($"Student Average Score is: {CalculateAverageScore}");
+  //      Console.WriteLine($"Student Letter Grade is: {GetGrade()}");
+  //  }
+  //  public void AddGrade(int grade)
+  //  {
+  //      if (grade >= 0 && grade <= 100)
+  //      {
+  //          ExamScores.Add(grade);
+  //          Console.WriteLine($"Added grade {grade} for {Name}");
+  //      }   // ^Ooh nice system, will have to use this more in the future
+  //      else
+  //      {
+  //          Console.WriteLine($"Invalid grade: {grade}. Grade must be between 0 and 100.");
+  //      }
+  //  }
+  //  public void DisplayAllGrades()
+  //  {
+  //      Console.WriteLine($"Grades for {Name}:");
+  //      for (int i = 0; i < ExamScores.Count; i++)
+  //      {
+  //          char letterGrade = GetLetterGrade(ExamScores[i]);
+  //          Console.WriteLine($"Exam {i + 1}: {letterGrade} ({ExamScores[i]})");
+  //      }
+  //  }
+  //  private char GetLetterGrade(int score)
+  //  {
+  //      if (score >= 90)
+  //      {
+  //          return 'A';
+  //      }
+  //      else if (score >= 80)
+  //      {
+  //          return 'B';
+  //      }
+  //     else if (score >= 70)
+  //      {
+  //          return 'C';
+  //      }
+  //      else if (score >= 60)
+  //      {
+  //          return 'D';
+  //      }
+  //      else
+  //      {
+  //          return 'F';
+  //      }
+  //  }
+//}
+//}
